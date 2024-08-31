c0a6126bfd3b556c6e6bb59466b61c1d45c88ca1
Gabriel Cabral <gabriel.cabral.medeiros@ccc.ufcg.edu.br>
2024-08-31 16:50:40 -0300

    arquivos

--- dist-newstyle/build/x86_64-linux/ghc-8.8.4/Hangman-Game-0.1.0.0/x/Hangman-Game/build/Hangman-Game/autogen/Paths_Hangman_Game.hs
+++ dist-newstyle/build/x86_64-linux/ghc-8.8.4/Hangman-Game-0.1.0.0/x/Hangman-Game/build/Hangman-Game/autogen/Paths_Hangman_Game.hs
@@ -1,23 +1,16 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE NoRebindableSyntax #-}
-#if __GLASGOW_HASKELL__ >= 810
-{-# OPTIONS_GHC -Wno-prepositive-qualified-module #-}
-#endif
 {-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
-{-# OPTIONS_GHC -w #-}
 module Paths_Hangman_Game (
     version,
     getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
     getDataFileName, getSysconfDir
   ) where
 
-
 import qualified Control.Exception as Exception
-import qualified Data.List as List
 import Data.Version (Version(..))
 import System.Environment (getEnv)
 import Prelude
-
 
 #if defined(VERSION_base)
 
@@ -34,44 +27,24 @@ catchIO = Exception.catch
 
 version :: Version
 version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/gabrielcabral/.cabal/bin"
+libdir     = "/home/gabrielcabral/.cabal/lib/x86_64-linux-ghc-8.8.4/Hangman-Game-0.1.0.0-inplace-Hangman-Game"
+dynlibdir  = "/home/gabrielcabral/.cabal/lib/x86_64-linux-ghc-8.8.4"
+datadir    = "/home/gabrielcabral/.cabal/share/x86_64-linux-ghc-8.8.4/Hangman-Game-0.1.0.0"
+libexecdir = "/home/gabrielcabral/.cabal/libexec/x86_64-linux-ghc-8.8.4/Hangman-Game-0.1.0.0"
+sysconfdir = "/home/gabrielcabral/.cabal/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "Hangman_Game_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "Hangman_Game_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "Hangman_Game_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "Hangman_Game_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "Hangman_Game_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "Hangman_Game_sysconfdir") (\_ -> return sysconfdir)
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName name = do
   dir <- getDataDir
-  return (dir `joinFileName` name)
-
-getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
-
-
-
-
-bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
-bindir     = "/home/arthur/.cabal/bin"
-libdir     = "/home/arthur/.cabal/lib/x86_64-linux-ghc-8.8.4/Hangman-Game-0.1.0.0-inplace-Hangman-Game"
-dynlibdir  = "/home/arthur/.cabal/lib/x86_64-linux-ghc-8.8.4"
-datadir    = "/home/arthur/.cabal/share/x86_64-linux-ghc-8.8.4/Hangman-Game-0.1.0.0"
-libexecdir = "/home/arthur/.cabal/libexec/x86_64-linux-ghc-8.8.4/Hangman-Game-0.1.0.0"
-sysconfdir = "/home/arthur/.cabal/etc"
-
-getBinDir     = catchIO (getEnv "Hangman_Game_bindir")     (\_ -> return bindir)
-getLibDir     = catchIO (getEnv "Hangman_Game_libdir")     (\_ -> return libdir)
-getDynLibDir  = catchIO (getEnv "Hangman_Game_dynlibdir")  (\_ -> return dynlibdir)
-getDataDir    = catchIO (getEnv "Hangman_Game_datadir")    (\_ -> return datadir)
-getLibexecDir = catchIO (getEnv "Hangman_Game_libexecdir") (\_ -> return libexecdir)
-getSysconfDir = catchIO (getEnv "Hangman_Game_sysconfdir") (\_ -> return sysconfdir)
-
-
-
-joinFileName :: String -> String -> FilePath
-joinFileName ""  fname = fname
-joinFileName "." fname = fname
-joinFileName dir ""    = dir
-joinFileName dir fname
-  | isPathSeparator (List.last dir) = dir ++ fname
-  | otherwise                       = dir ++ pathSeparator : fname
-
-pathSeparator :: Char
-pathSeparator = '/'
-
-isPathSeparator :: Char -> Bool
-isPathSeparator c = c == '/'
+  return (dir ++ "/" ++ name)
--- dist-newstyle/build/x86_64-linux/ghc-8.8.4/Hangman-Game-0.1.0.0/x/Hangman-Game/build/Hangman-Game/autogen/cabal_macros.h
+++ dist-newstyle/build/x86_64-linux/ghc-8.8.4/Hangman-Game-0.1.0.0/x/Hangman-Game/build/Hangman-Game/autogen/cabal_macros.h
@@ -10,6 +10,7 @@
   (major1) == 0 && (major2) <  1 || \
   (major1) == 0 && (major2) == 1 && (minor) <= 0)
 #endif /* MIN_VERSION_Hangman_Game */
+
 /* package ansi-terminal-1.1.1 */
 #ifndef VERSION_ansi_terminal
 #define VERSION_ansi_terminal "1.1.1"
@@ -20,6 +21,7 @@
   (major1) == 1 && (major2) <  1 || \
   (major1) == 1 && (major2) == 1 && (minor) <= 1)
 #endif /* MIN_VERSION_ansi_terminal */
+
 /* package base-4.13.0.0 */
 #ifndef VERSION_base
 #define VERSION_base "4.13.0.0"
@@ -30,6 +32,7 @@
   (major1) == 4 && (major2) <  13 || \
   (major1) == 4 && (major2) == 13 && (minor) <= 0)
 #endif /* MIN_VERSION_base */
+
 /* package mysql-simple-0.4.9 */
 #ifndef VERSION_mysql_simple
 #define VERSION_mysql_simple "0.4.9"
@@ -40,6 +43,7 @@
   (major1) == 0 && (major2) <  4 || \
   (major1) == 0 && (major2) == 4 && (minor) <= 9)
 #endif /* MIN_VERSION_mysql_simple */
+
 /* package random-1.2.1.2 */
 #ifndef VERSION_random
 #define VERSION_random "1.2.1.2"
@@ -50,6 +54,7 @@
   (major1) == 1 && (major2) <  2 || \
   (major1) == 1 && (major2) == 2 && (minor) <= 1)
 #endif /* MIN_VERSION_random */
+
 /* package text-1.2.4.0 */
 #ifndef VERSION_text
 #define VERSION_text "1.2.4.0"
@@ -71,6 +76,7 @@
   (major1) == 11 && (major2) <  0 || \
   (major1) == 11 && (major2) == 0 && (minor) <= 0)
 #endif /* MIN_TOOL_VERSION_gcc */
+
 /* tool ghc-8.8.4 */
 #ifndef TOOL_VERSION_ghc
 #define TOOL_VERSION_ghc "8.8.4"
@@ -81,6 +87,7 @@
   (major1) == 8 && (major2) <  8 || \
   (major1) == 8 && (major2) == 8 && (minor) <= 4)
 #endif /* MIN_TOOL_VERSION_ghc */
+
 /* tool ghc-pkg-8.8.4 */
 #ifndef TOOL_VERSION_ghc_pkg
 #define TOOL_VERSION_ghc_pkg "8.8.4"
@@ -91,6 +98,7 @@
   (major1) == 8 && (major2) <  8 || \
   (major1) == 8 && (major2) == 8 && (minor) <= 4)
 #endif /* MIN_TOOL_VERSION_ghc_pkg */
+
 /* tool haddock-2.23.0 */
 #ifndef TOOL_VERSION_haddock
 #define TOOL_VERSION_haddock "2.23.0"
@@ -101,6 +109,7 @@
   (major1) == 2 && (major2) <  23 || \
   (major1) == 2 && (major2) == 23 && (minor) <= 0)
 #endif /* MIN_TOOL_VERSION_haddock */
+
 /* tool hpc-0.67 */
 #ifndef TOOL_VERSION_hpc
 #define TOOL_VERSION_hpc "0.67"
@@ -111,6 +120,7 @@
   (major1) == 0 && (major2) <  67 || \
   (major1) == 0 && (major2) == 67 && (minor) <= 0)
 #endif /* MIN_TOOL_VERSION_hpc */
+
 /* tool hsc2hs-0.68.7 */
 #ifndef TOOL_VERSION_hsc2hs
 #define TOOL_VERSION_hsc2hs "0.68.7"
@@ -121,16 +131,7 @@
   (major1) == 0 && (major2) <  68 || \
   (major1) == 0 && (major2) == 68 && (minor) <= 7)
 #endif /* MIN_TOOL_VERSION_hsc2hs */
-/* tool pkg-config-0.29.2 */
-#ifndef TOOL_VERSION_pkg_config
-#define TOOL_VERSION_pkg_config "0.29.2"
-#endif /* TOOL_VERSION_pkg_config */
-#ifndef MIN_TOOL_VERSION_pkg_config
-#define MIN_TOOL_VERSION_pkg_config(major1,major2,minor) (\
-  (major1) <  0 || \
-  (major1) == 0 && (major2) <  29 || \
-  (major1) == 0 && (major2) == 29 && (minor) <= 2)
-#endif /* MIN_TOOL_VERSION_pkg_config */
+
 /* tool runghc-8.8.4 */
 #ifndef TOOL_VERSION_runghc
 #define TOOL_VERSION_runghc "8.8.4"
@@ -141,6 +142,7 @@
   (major1) == 8 && (major2) <  8 || \
   (major1) == 8 && (major2) == 8 && (minor) <= 4)
 #endif /* MIN_TOOL_VERSION_runghc */
+
 /* tool strip-2.38 */
 #ifndef TOOL_VERSION_strip
 #define TOOL_VERSION_strip "2.38"
